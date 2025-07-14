6c5e65a7579dcd63e90c36cd60560f81381fc9e2
aka-nse <nakase.d1220@gmail.com>
2025-07-15 00:05:48 +0900

    adds more tests

--- src/QuantitiesDotNet.Test/ParseTest.cs
+++ src/QuantitiesDotNet.Test/ParseTest.cs
@@ -5,40 +5,77 @@ namespace QuantitiesDotNet;
 public class ParseTest
 {
 #pragma warning disable format
-    public static IEnumerable<object?[]> ParseTestCase()
+    public static TheoryData<string?, bool, XunitSerializable<QuantityParseInfo?>> TryCompileTestCases()
+        => new()
+        {
+            { null, false, new(null) },
+            { "", false, new(null) },
+            { " ", false, new(null) },
+            { "1.23m/s", true, new(new QuantityParseInfo("1.23", "m/s")) },
+            { "1.23 m/s", true, new(new QuantityParseInfo("1.23", "m/s")) },
+            { "1.23[m/s]", true, new(new QuantityParseInfo("1.23", "m/s")) },
+            { "1.23 [m/s]", true, new(new QuantityParseInfo("1.23", "m/s")) },
+            { "1.23 [m/s", false, new(null) },
+            { "1.23m^2/s^2", true, new(new QuantityParseInfo("1.23", "m^2/s^2")) },
+            { "-1.23kg*m/s^2", true, new(new QuantityParseInfo("-1.23", "kg*m/s^2")) },
+            { "+1.23kg*m/s^2", true, new(new QuantityParseInfo("+1.23", "kg*m/s^2")) },
+            { "1.23E+3m/s", true, new(new QuantityParseInfo("1.23E+3", "m/s")) },
+            { "1.23E-3m/s", true, new(new QuantityParseInfo("1.23E-3", "m/s")) },
+            { "1,23m/s", true, new(new QuantityParseInfo("1,23", "m/s")) },
+            { "123", false, new(null) },
+            { "m/s", false, new(null) },
+            { "1.23", false, new(null) },
+            { "1.23 [m s^-1]", true, new(new QuantityParseInfo("1.23", "m s^-1")) },
+            { "1.23 [m s^-1", false, new(null) },
+            { "1.23 [m/s] extra", false, new(null) },
+        };
+
+
+#pragma warning restore format
+
+    [Theory]
+    [MemberData(nameof(TryCompileTestCases))]
+    public void TryCompile(string? expression, bool expected, XunitSerializable<QuantityParseInfo?> expectedInfo)
     {
-        static object?[] core(string expression, QSpeed? expected)
-            => new object?[] { expression, expected, };
-
-        yield return core("1.234m/s"           , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234m/s"           , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234 m/s"          , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234[m/s]"         , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234 [m/s]"        , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234000E+000m/s"   , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234000E+000m/s"   , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234000E+000 m/s"  , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234000E+000[m/s]" , QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234000E+000 [m/s]", QSpeed.FromMetrePerSecond(1.234));
-        yield return core("1.234000E+000 [m/s", null);
-
-        yield break;
+        if (expected)
+        {
+            Assert.True(QuantityParseInfo.TryCompile(expression, out var info));
+            Assert.Equal(expectedInfo.Value, info);
+        }
+        else
+        {
+            Assert.False(QuantityParseInfo.TryCompile(expression, out _));
+        }
     }
 
-    public static IEnumerable<object?[]> ParseGenericTestCase()
-    {
-        static object?[] core(string expression, QSpeed<decimal>? expected)
-            => new object?[] { expression, expected, };
 
-        yield return core("1.234m/s"           , QSpeed<decimal>.FromMetrePerSecond(1.234m));
-        yield return core("1.234m/s"           , QSpeed<decimal>.FromMetrePerSecond(1.234m));
-        yield return core("1.234 m/s"          , QSpeed<decimal>.FromMetrePerSecond(1.234m));
-        yield return core("1.234[m/s]"         , QSpeed<decimal>.FromMetrePerSecond(1.234m));
-        yield return core("1.234 [m/s]"        , QSpeed<decimal>.FromMetrePerSecond(1.234m));
-        yield return core("1.234 [m/s" , null);
+#pragma warning disable format
+    public static TheoryData<string, QSpeed?> ParseTestCase()
+        => new()
+        {
+            { "1.234m/s"           , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234m/s"           , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234 m/s"          , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234[m/s]"         , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234 [m/s]"        , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234000E+000m/s"   , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234000E+000m/s"   , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234000E+000 m/s"  , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234000E+000[m/s]" , QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234000E+000 [m/s]", QSpeed.FromMetrePerSecond(1.234) },
+            { "1.234000E+000 [m/s", null },
+        };
 
-        yield break;
-    }
+    public static TheoryData<string, QSpeed<decimal>?> ParseGenericTestCase()
+        => new()
+        {
+            { "1.234m/s"           , QSpeed<decimal>.FromMetrePerSecond(1.234m) },
+            { "1.234m/s"           , QSpeed<decimal>.FromMetrePerSecond(1.234m) },
+            { "1.234 m/s"          , QSpeed<decimal>.FromMetrePerSecond(1.234m) },
+            { "1.234[m/s]"         , QSpeed<decimal>.FromMetrePerSecond(1.234m) },
+            { "1.234 [m/s]"        , QSpeed<decimal>.FromMetrePerSecond(1.234m) },
+            { "1.234 [m/s" , null },
+        };
 #pragma warning restore format
 
     [Theory]
