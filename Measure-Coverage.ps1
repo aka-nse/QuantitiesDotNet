4e3f32566a112d24cc83b76b65ef318fca2889a0
Daishi Nakase <nakase.d1220@gmail.com>
2026-01-24 15:33:16 +0900

    fixes scripts

--- Measure-Coverage.ps1
+++ Measure-Coverage.ps1
@@ -15,10 +15,10 @@ try {
     Get-ChildItem -Directory src/*.Test*/TestResults/* | Remove-Item -Recurse
 
     # rebuild target project to generate source generator files
-    dotnet build src/QuantitiesDotNet.slnx --no-incremental --property:EmitCompilerGeneratedFiles=true
+    dotnet build QuantitiesDotNet.slnx --no-incremental --property:EmitCompilerGeneratedFiles=true
 
     # test and measure coverage
-    dotnet test src/QuantitiesDotNet.slnx --collect:"XPlat Code Coverage" --settings src/etc/coverlet.runsettings
+    dotnet test QuantitiesDotNet.slnx --collect:"XPlat Code Coverage" --settings src/etc/coverlet.runsettings
 
     # export HTML coverage report
     Get-ChildItem src/*.Test*/TestResults/*/coverage.cobertura.xml `
